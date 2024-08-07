a96e48cfad1067e32aef76c544eb5d03d20debf0
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-07 08:09:33 -0300

    feat: add environment variables

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -1,9 +1,10 @@
+import { API_KEY_TMDB, API_URL_TMDB } from '@env'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL: API_URL_TMDB,
   params: {
-    api_key: "",
+    api_key: API_KEY_TMDB,
     language: "pt-BR",
     include_adults: false,
   },
