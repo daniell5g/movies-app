86a68f37827c9ed3761ae83097938f9f25f4e283
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-07 00:21:35 -0300

    feat: create component for the logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+import logoPng from '../../assets/images/icon.png'
+import * as S from './styles'
+
+export const ImageLogo = () => {
+  return (
+    <S.ImageLogo source={logoPng} alt='Letras BRQ em branco e o a palavra movies em amarelo' />
+  )
+}
