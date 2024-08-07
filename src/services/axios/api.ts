f67395e9f1e913d4c27c8d173a955f2284c1c6a8
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-07 05:45:06 -0300

    feat: create config initial api movies the movie db

---
+++ src/services/axios/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "",
+    language: "pt-BR",
+    include_adults: false,
+  },
+});
