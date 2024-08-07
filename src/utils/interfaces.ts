2d04e9e3534a2615c38a43afe1627ba768c61536
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-07 07:10:40 -0300

    feat: add news fields

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,7 +1,12 @@
 export interface Movie {
   id: number;
   title: string;
-  poster_path: string;
   overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  runtime: number;
+  release_date: string;
   vote_average: number;
+  vote_count: number;
+  revenue: number;
 }
