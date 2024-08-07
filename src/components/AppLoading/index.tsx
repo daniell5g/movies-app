2853c3d5db9830cee5aa9ba09e56abdb17c738b4
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-07 00:22:05 -0300

    refactor: uses logo component import

--- src/components/AppLoading/index.tsx
+++ src/components/AppLoading/index.tsx
@@ -1,13 +1,12 @@
+import { ImageLogo } from '@components/Logo'
 import React from 'react'
-
-import logoPng from '@/assets/images/icon.png'
 
 import * as S from './styles'
 
 export const AppLoading = () => {
   return (
     <S.LoadingContainer>
-      <S.LoadingImage source={logoPng} alt='Letras BRQ em branco e o a palavra movies em amarelo' />
+      <ImageLogo />
       <S.LoadingIndicator />
     </S.LoadingContainer>
   )
