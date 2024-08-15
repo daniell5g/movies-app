62adacd08dc64f62f35045ce68206a750d3cc74a
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-15 12:32:10 -0300

    refact: removes details page page rules and liability

---
+++ src/pages/DetailsPage/viewModel/interface.ts
@@ -0,0 +1,9 @@
+import type { Movie } from '@utils/interfaces'
+
+export interface IDetailsPageViewModel {
+  loading: boolean
+  isFavorite: boolean
+  movieDetails: Movie | null
+  handleGoBack: () => void
+  handleToggleFavorite: () => void
+}
