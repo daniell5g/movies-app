9187868dc2e635b6cc0889a2f850b459f628fbf3
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-14 22:47:14 -0300

    chore: new jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,33 @@
 module.exports = {
-  preset: 'react-native',
-  setupFiles: ['./jest.setup.js'],
+  preset: 'jest-expo',
+  setupFiles: ['./jest.setup.js'], 
   setupFilesAfterEnv: [
-    '<rootDir>/__mocks__/globalMock.js'
+    '<rootDir>/__mocks__/globalMock.js', 
   ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', 
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|@react-native|@testing-library|@react-navigation|@react-native-community)"
-  ],
+    "node_modules/(?!(jest-)?react-native|@react-native|@testing-library|@react-navigation|@react-native-community|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules|unimodules|sentry-expo|native-base|react-clone-referenced-element|@react-native-picker|@react-native-community|@expo/vector-icons|expo-font|expo-constants)"
+  ], 
   testMatch: [
-    '**/__tests__/**/*.test.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)',
+    '**/__tests__/**/*.test.[jt]s?(x)', 
+    '**/?(*.)+(spec|test).[jt]s?(x)', 
   ],
-  collectCoverage: true,
-  coverageDirectory: '__tests__/coverage',
+  collectCoverage: false, 
+  coverageDirectory: '__tests__/coverage', 
   collectCoverageFrom: [
-    './src/components/*.{ts,tsx}',
-    './src/hooks/*.{ts,tsx}',
-    './src/pages/*.{ts,tsx}',
-    './src/libs/*.{ts,tsx}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/babel.config.js',
-    '!**/jest.config.js',
-    '!**/src/config/**',
-    '!**/ReactotronConfig.js',
-    '!**/src/routes/**',
-    '!**/jest.config.js',
-    '!**/prettier.config.js',
+    './src/components/*.{ts,tsx}', 
+    './src/hooks/*.{ts,tsx}', 
+    './src/pages/*.{ts,tsx}', 
+    './src/libs/*.{ts,tsx}', 
+    '!**/coverage/**', 
+    '!**/node_modules/**', 
+    '!**/babel.config.js', 
+    '!**/jest.config.js', 
+    '!**/src/config/**', 
+    '!**/ReactotronConfig.js', 
+    '!**/src/routes/**', 
+    '!**/prettier.config.js', 
   ]
 };
--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,7 @@ jest.mock('@expo/vector-icons', () => ({
   Feather: '',
   MaterialIcons: ''
 }))
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
