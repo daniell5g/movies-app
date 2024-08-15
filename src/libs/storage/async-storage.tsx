ecb191354aa91e8964bffc9b772c1784e7cb6a80
Daniel Marques <daniel.silva.city@gmail.com>
2024-08-15 15:26:07 -0300

    refactor: remove unnecessary snippets

--- src/libs/storage/async-storage.tsx
+++ src/libs/storage/async-storage.tsx
@@ -4,27 +4,14 @@ import { IStorage } from './index';
 
 export class AsyncStorageImpl implements IStorage {
   async getItem(key: string): Promise<string | null> {
-    try {
-      return await AsyncStorage.getItem(key);
-    } catch (error) {
-      console.error('AsyncStorage#getItem error:', error);
-      return null;
-    }
+    return await AsyncStorage.getItem(key);
   }
 
   async setItem(key: string, value: string): Promise<void> {
-    try {
-      await AsyncStorage.setItem(key, value);
-    } catch (error) {
-      console.error('AsyncStorage#setItem error:', error);
-    }
+    await AsyncStorage.setItem(key, value);
   }
 
   async removeItem(key: string): Promise<void> {
-    try {
-      await AsyncStorage.removeItem(key);
-    } catch (error) {
-      console.error('AsyncStorage#removeItem error:', error);
-    }
+    await AsyncStorage.removeItem(key);
   }
 }
